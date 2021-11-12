7bd0e1c9552dcc28ea0fb9d4a4e532c749de79f0
lidongYang22 <73336661+lidongYang22@users.noreply.github.com>
2021-11-12 20:09:17 +0800

    Add files via upload

---
+++ DNN training/dataset_generation_for_orientation_angle.py
@@ -0,0 +1,43 @@
+
+import os
+import math
+from torch.utils.data import DataLoader
+import matplotlib
+
+def maze_shuffle():
+
+    files = os.listdir('./angle_output_rotate_situation')
+
+    file_list = DataLoader(files, shuffle = True, batch_size = 1)
+
+    n = len(files)
+
+    print(n)
+
+    count_train = 0
+    count_test = 0
+
+    train = []
+    test = []
+
+    f = open('./train_dataset.txt', 'w').close()
+    f = open('./test_dataset.txt', 'w').close()
+
+    for i,name in enumerate(file_list):
+
+        if i < math.ceil(0.9 * n):
+            train.append(name[0])
+            count_train = count_train + 1
+        else:
+            test.append(name[0])
+            count_test = count_test + 1
+
+    f = open('./train_dataset.txt', 'a')
+    for i in range (count_train):
+        f.write(train[i] + '\n')
+    f.close()
+
+    f = open('./test_dataset.txt', 'a')
+    for i in range (count_test):
+        f.write(test[i] + '\n')
+    f.close()
